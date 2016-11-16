aba3f269cbcb45708f09beb0b33292b672aa53b2
Egli Tobias, GHR-DEV-NEX-R1-BE5 <Tobias.Egli@swisscom.com>
2016-11-16 17:06:39 +0100

    Implemented basic methods

--- src/Highscore.java
+++ src/Highscore.java
@@ -5,12 +5,21 @@ import java.util.Map;
  * Created by Tobias on 09.11.2016.
  */
 public class Highscore {
+    private SQLiteJDBC dataAccess = new SQLiteJDBC();
+
     public void setHighscores(HashMap<String, Integer> highscores) {
-        for(Map.Entry<String, Integer> highscore : highscores.entrySet()) {
+        for (Map.Entry<String, Integer> highscore : highscores.entrySet()) {
             String playerName = highscore.getKey();
             Integer score = highscore.getValue();
-            if (checkPlayerNamePResent(playerName)) {
-                updateHighscore(playerName, score);
+
+            HashMap<String, Integer> player = getPlayer(playerName);
+
+            if (player != null) {
+                Map.Entry<String,Integer> entry=player.entrySet().iterator().next();
+                Integer oldPlayerScore=entry.getValue();
+                if (oldPlayerScore < score) {
+                    updateHighscore(playerName, score);
+                }
             } else {
                 insertHighscore(playerName, score);
             }
@@ -18,18 +27,38 @@ public class Highscore {
     }
 
     public HashMap<String, Integer> getHighscores() {
-        return new HashMap<String, Integer>();
+        return dataAccess.selectData("SELECT * FROM highscore");
     }
 
-    private boolean checkPlayerNamePResent (String playerName) {
-        return true;
+    private HashMap<String, Integer> getPlayer(String playerName) {
+        HashMap<String, Integer> result = dataAccess.selectData("SELECT * FROM highscore WHERE name = '" + playerName + "'");
+        if (result.size() > 0) {
+            return result;
+        }
+        return null;
     }
 
-    private void updateHighscore(String playerName, int Score) {
-
+    private void updateHighscore(String playerName, int score) {
+        dataAccess.updateData(playerName, score);
     }
 
-    private void insertHighscore(String playerName, int Score) {
+    private void insertHighscore(String playerName, int score) {
+        dataAccess.insertData(playerName, score);
+    }
 
+    public static void main(String[] args) {
+        Highscore hs = new Highscore();
+        SQLiteJDBC sq = new SQLiteJDBC();
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("Tobias", 301);
+        hs.setHighscores(hm);
+
+        HashMap<String, Integer> r = sq.selectData("SELECT * FROM highscore");
+
+        for (Map.Entry<String, Integer> entry : r.entrySet()) {
+            String key = entry.getKey().toString();
+            Integer value = entry.getValue();
+            System.out.println("key, " + key + " value " + value);
+        }
     }
 }
--- src/SQLiteJDBC.java
+++ src/SQLiteJDBC.java
@@ -1,22 +1,65 @@
 /**
  * Created by jajaj on 09.11.2016.
  */
+import org.sqlite.SQLiteConfig;
+
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SQLiteJDBC {
-    public static void main( String args[] )
-    {
-        Connection c = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:highscore.db");
-            Statement stm = c.createStatement();
-            stm.execute("");
 
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:highscore.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
-        System.out.println("Opened database successfully");
+        return conn;
     }
+
+    public HashMap<String, Integer> selectData(String sql){
+        HashMap result = new HashMap();
+
+        try (Connection conn = connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                result.put(rs.getString("name"), rs.getInt("score"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+
+    public void insertData(String playerName, Integer score) {
+        try
+        {
+            Connection conn = connect();
+            Statement stmt  = conn.createStatement();
+            stmt.executeQuery("INSERT INTO highscore (name, score) VALUES ('" +
+                    playerName + "', " + score + ")");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void updateData(String playerName, Integer score) {
+        try
+        {
+            Connection conn = connect();
+            Statement stmt  = conn.createStatement();
+            stmt.executeQuery("UPDATE highscore SET score=" + score +
+                    " WHERE name='" + playerName + "'");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
