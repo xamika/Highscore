a2cec868532e344a223beaa75d979085d48d2848
Egli Tobias, GHR-DEV-NEX-R1-BE5 <Tobias.Egli@swisscom.com>
2016-12-14 15:55:38 +0100

    Implemented basic methods

--- src/highscore/Highscore.java
+++ src/highscore/Highscore.java
@@ -33,29 +33,15 @@ public class Highscore implements HighscoreServer {
         if (player.size() == 0) {
             dataAccess.insertData(playerName, score);
         } else {
-            updateIfHigherScore(player, playerName, score);
+            updateScore(player, playerName, score);
         }
     }
 
-    private void updateIfHigherScore(HashMap<String, Integer> player, String playerName, Integer score) {
+    private void updateScore(HashMap<String, Integer> player, String playerName, Integer score) {
         Map.Entry<String, Integer> entry = player.entrySet().iterator().next();
         Integer oldPlayerScore = entry.getValue();
-        if (oldPlayerScore < score) {
-            dataAccess.updateData(playerName, score);
-        }
-    }
-
-    public static void main(String[] args) {
-    	Highscore hs = new Highscore();
-		
-        HashMap<String, Integer> r = hs.getHighscoreByPlayerName("Tobias");
-
-        for (Map.Entry<String, Integer> entry : r.entrySet()) {
-            String key = entry.getKey().toString();
-            Integer value = entry.getValue();
-            System.out.println("key, " + key + " value " + value);
-        }
-  
+        score = oldPlayerScore + score;
+        dataAccess.updateData(playerName, score);
     }
     
 }
